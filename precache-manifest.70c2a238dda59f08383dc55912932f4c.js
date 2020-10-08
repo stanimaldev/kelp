036a370363f782b082e9e9288d865ee68ccaf293
Stanley Jeong <jeongstanley@gmail.com>
2020-10-07 22:44:00 -0700

    Updates

---
+++ precache-manifest.70c2a238dda59f08383dc55912932f4c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2b812ca06013eb35a96c3e31874f6a",
+    "url": "/kelp/index.html"
+  },
+  {
+    "revision": "9c8c9071ad894aa80b31",
+    "url": "/kelp/static/css/main.963a2346.chunk.css"
+  },
+  {
+    "revision": "b85d0a9dc883540f9f71",
+    "url": "/kelp/static/js/2.17ee7728.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kelp/static/js/2.17ee7728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c8c9071ad894aa80b31",
+    "url": "/kelp/static/js/main.dd2a7f54.chunk.js"
+  },
+  {
+    "revision": "2284dda81b3aa5ed853c",
+    "url": "/kelp/static/js/runtime-main.c3ad113a.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/kelp/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/kelp/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "83942fdc46cb8b6a22ca6f6bd2b1e428",
+    "url": "/kelp/static/media/favicon.83942fdc.ico"
+  },
+  {
+    "revision": "062a796bde2bd7332ddff86c322e927d",
+    "url": "/kelp/static/media/kelp.062a796b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ea93ce5599419cea235262735a78c2fc.js"
+  "/kelp/precache-manifest.70c2a238dda59f08383dc55912932f4c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kelp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
